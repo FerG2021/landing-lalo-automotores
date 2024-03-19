7f68445ed5ff4773b7996fd4cad03bde02ca4611
Fernando Gonzalez <fernandojaviergonzalez2018@gmail.com>
2024-03-19 15:52:15 -0300

    correccion de store

--- src/managers/store/store.js
+++ src/managers/store/store.js
@@ -2,4 +2,8 @@ import { createStore } from 'vuex';
 
 import carsStore from '../../modules/cars/carsStore';
 
-export default createStore({ CarsStore: carsStore });
+export default createStore({
+	modules: {
+		CarsStore: carsStore
+	}
+});
